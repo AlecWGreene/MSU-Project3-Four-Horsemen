5a36941a58c96c85de1719c4e2509497319444dc
Alec W Greene <alecwgreene@gmail.com>
2020-11-09 19:28:10 -0500

    Implemented ImgData objects in SpriteEnum.js as a object whose keys are the sprite identifiers and values are the associated image data

--- revelations/client/src/Game/Animator/Sprite.js
+++ revelations/client/src/Game/Animator/Sprite.js
@@ -1,5 +1,25 @@
 import React from "react";
+import styled from "styled-components";
 
+/**
+ * @example
+ * <Frame height={120} width={120} rotation={45} scale={0.75}> ... </Frame>
+ */
+const Frame = styled.div``;
+
+/**
+ * @example
+ * <Image src={"./Assets/spaceship.png"} left={90} />
+ */
+const Image = styled.object``;
+
+/**
+ * @param {{src: string, height: number, width: number, rotation: number, scale: number, offset: number}} props
+ */
 function Sprite(props){
-return <div>sprite placeholder</div>
+    return <Frame>
+        <Image src={props.src} type={"image/svg+xml"} />
+    </Frame>
 }
+
+export default Sprite;
