6072e5d698eb262a3781bc5b4426319609a98f96
Alec W Greene <alecwgreene@gmail.com>
2020-11-12 17:30:11 -0500

    Fixed up game logic and styling for Wall and Creep layers. Setup initialization very jankily for gameManager, does not currently run sendWave but will run testGame in the console

--- revelations/client/src/Game/CreepLayer/creep.js
+++ revelations/client/src/Game/CreepLayer/creep.js
@@ -11,11 +11,10 @@ const styles = {
 }
 
 export default function CreepLayer(props){
-
     return (
         <div style={styles.container}>
         {
-            //Object.entries(props.creepDirectory).map(creep => <Animator imgData={SPRITE_ENUM["Creep_1_RED"]} position={creep[1].transform.position} rotation={creep[1].transform.rotation * 180 / Math.PI} scale={1} key={creep[0]}/>)
+            Object.entries(props.creep).map(creep => <Animator imgData={SPRITE_ENUM["Creep_1_RED"]} position={creep[1].transform.position} rotation={creep[1].transform.rotation * 180 / Math.PI} scale={1} key={creep[0]}/>)
         }
         </div>
     )   
